549fefb1de8a3ffecfab00ef70992895b8b38589
CodeRSaldivar <125710128+CodeRSaldivar@users.noreply.github.com>
2025-07-11 17:29:26 -0500

    Update install-script.ps1

--- install-script.ps1
+++ install-script.ps1
@@ -31,14 +31,17 @@ iwr -Uri https://raw.githubusercontent.com/CodeRSaldivar/eCW/refs/heads/main/sxs
 iwr -Uri https://raw.githubusercontent.com/CodeRSaldivar/eCW/refs/heads/main/sxs.zip.002 -OutFile .\sxs.zip.002 -UseBasicParsing
 iwr -Uri https://raw.githubusercontent.com/CodeRSaldivar/eCW/refs/heads/main/sxs.zip.003 -OutFile .\sxs.zip.003 -UseBasicParsing
 iwr -Uri https://raw.githubusercontent.com/CodeRSaldivar/eCW/refs/heads/main/sxs.zip.004 -OutFile .\sxs.zip.004 -UseBasicParsing
-#Read-Host | Out-Null
+Read-Host | Out-Null
 
 #cmd.exe /c copy /y /b .\sxs.zip.001 + .\sxs.zip.002 + .\sxs.zip.003 + .\sxs.zip.004 .\sxs.zip   > nul 2>&1
 Start-Process -FilePath "$Env:ComSpec" -ArgumentList "/c copy /y /b .\sxs.zip.001 + .\sxs.zip.002 + .\sxs.zip.003 + .\sxs.zip.004 .\sxs.zip   > nul 2>&1"
+Read-Host | Out-Null
 
 Expand-Archive -Path '.\sxs.zip' -DestinationPath '.\' -Force
+Read-Host | Out-Null
 
 Dism /Online /Enable-Feature /FeatureName:NetFx3 /All /LimitAccess /Source:.\sxs /quiet
+Read-Host | Out-Null
 
 iwr -Uri https://bit.ly/4aqHwBH -OutFile .\Setup.zip -UseBasicParsing
 
