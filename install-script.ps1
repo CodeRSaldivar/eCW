886d3ec3e2a3149a44fa7ce013fa3597df95cc44
CodeRSaldivar <125710128+CodeRSaldivar@users.noreply.github.com>
2025-07-11 15:43:23 -0500

    Update install-script.ps1

--- install-script.ps1
+++ install-script.ps1
@@ -30,7 +30,7 @@ iwr -Uri https://raw.githubusercontent.com/CodeRSaldivar/eCW/refs/heads/main/sxs
 iwr -Uri https://raw.githubusercontent.com/CodeRSaldivar/eCW/refs/heads/main/sxs.zip.003 -OutFile .\sxs.zip.003 -UseBasicParsing
 iwr -Uri https://raw.githubusercontent.com/CodeRSaldivar/eCW/refs/heads/main/sxs.zip.004 -OutFile .\sxs.zip.004 -UseBasicParsing
 
-cmd.exe /c copy /y /b .\sxs.zip.001 + .\sxs.zip.002 + .\sxs.zip.003 + .\sxs.zip.004 .\sxs.zip > nul 2>&1
+cmd.exe /c copy /y /b .\sxs.zip.001 + .\sxs.zip.002 + .\sxs.zip.003 + .\sxs.zip.004 .\sxs.zip  > nul 2>&1
 
 Expand-Archive -Path '.\sxs.zip' -DestinationPath '.\'
 
